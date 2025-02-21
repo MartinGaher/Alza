2d4e966fa59671b5ec0972bd654fde5e74d8dc66
Martin Gahér <martin.gaher@tietoevry.com>
2025-02-21 18:46:42 +0100

    swagger config

--- EShop.Api/Program.cs
+++ EShop.Api/Program.cs
@@ -1,16 +1,12 @@
-using Asp.Versioning;
+using EShop.Api.Swagger;
 using EShop.Core.Factories;
 using EShop.Infrastructure.Factories;
 using EShop.Mocks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
-using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
 // Configure Entity Framework Core with SQL Server
@@ -46,60 +42,14 @@ builder.Services.AddScoped<IProductRepository>(provider =>
 // Dependency Injection
 builder.Services.AddScoped<IProductService, ProductService>();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddApiVersioning(options =>
-{
-    options.DefaultApiVersion = new ApiVersion(1, 0);
-    options.AssumeDefaultVersionWhenUnspecified = true;
-    options.ReportApiVersions = true;
-});
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "EShop API", Version = "v1" });
-    c.SwaggerDoc("v2", new OpenApiInfo { Title = "EShop API", Version = "v2" });
-
-    // Include XML comments if applicable
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"; // Adjust if necessary
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath);
-
-    // Add a versioned API endpoint
-    c.DocInclusionPredicate((version, apiDescription) =>
-    {
-        var actionApiVersionModel = apiDescription.ActionDescriptor
-            .EndpointMetadata
-            .OfType<ApiVersionAttribute>()
-            .FirstOrDefault();
-
-        if (actionApiVersionModel != null)
-        {
-            return actionApiVersionModel.Versions.Any(v => $"v{v.MajorVersion}" == version);
-        }
-
-        return false;
-    });
-});
-
+// Configure Swagger and API versioning
+builder.Services.AddSwaggerServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        c.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
-    });
-}
+app.UseSwaggerMiddleware(app.Environment);
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ EShop.Api/Swagger/SwaggerConfig.cs
@@ -0,0 +1,70 @@
+﻿using Asp.Versioning;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace EShop.Api.Swagger
+{
+    /// <summary>
+    /// Swagger config
+    /// </summary>
+    public static class SwaggerConfig
+    {
+        /// <summary>
+        /// Configuring Swagger
+        /// </summary>
+        public static void AddSwaggerServices(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EShop API", Version = "v1" });
+                c.SwaggerDoc("v2", new OpenApiInfo { Title = "EShop API", Version = "v2" });
+
+                // Include XML comments if applicable
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"; // Adjust if necessary
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+                // Add a versioned API endpoint
+                c.DocInclusionPredicate((version, apiDescription) =>
+                {
+                    var actionApiVersionModel = apiDescription.ActionDescriptor
+                        .EndpointMetadata
+                        .OfType<ApiVersionAttribute>()
+                        .FirstOrDefault();
+
+                    if (actionApiVersionModel != null)
+                    {
+                        return actionApiVersionModel.Versions.Any(v => $"v{v.MajorVersion}" == version);
+                    }
+
+                    return false;
+                });
+            });
+        }
+
+        /// <summary>
+        /// Configure the HTTP request pipeline.
+        /// </summary>
+        public static void UseSwaggerMiddleware(this IApplicationBuilder app, IHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    c.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
+                });
+            }
+        }
+
+    }
+}
